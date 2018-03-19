d5529968fe8d3799bc042aad1b9cd46dc2d90d15
LaoBan-ywcm <249007415@qq.com>
2018-03-19 22:20:38 +0800

    feat:添加数据库配置

--- hello.py
+++ hello.py
@@ -3,18 +3,52 @@
     Author: LaoBan-ywcm
     Date:   2018-03-13 22:14:43
     Last Modified by:   LaoBan-ywcm
-    Last Modified time: 2018-03-15 21:29:24
+    Last Modified time: 2018-03-19 22:20:09
 '''
 from datetime import datetime
 
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLALchemy(app)
+
+class NameForm(Form):
+    name = StringField('what is you name?', validators=[Required()])
+    submit = SubmitField('submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.errorhandler(404)
@@ -25,11 +59,17 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user_agent = request.headers.get('User-Agent')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('看起来你修改了名字')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html',
-        current_time=datetime.utcnow())
+        form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
